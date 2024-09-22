1449d9c9755850a285e5d1ab1e824124000e8c29
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-09-22 17:50:14 +0400

    NP 07 FTP

---
+++ NP 07. FTP Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+getFtp();
+void getFtp()
+{
+    var request = WebRequest.Create("ftp://13.88.206.233") as FtpWebRequest;
+    request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+    var response = request.GetResponse() as FtpWebResponse;
+    var stream = response.GetResponseStream();
+    var sr = new StreamReader(stream);
+    var data = sr.ReadToEnd();
+    Console.WriteLine(data);
+}
